4be70ec504c01b28726e881b93eb9c661fc5d69b
Mike Bridge <mikebridge116@gmail.com>
2018-03-17 08:34:14 -0400

    fix response headers so they are available before sent

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use('/users', users);
 //Use API Routes
 app.use('/api', api);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -51,6 +52,7 @@ app.use(function(err, req, res, next) {
 //////////
 ///Custom
 //////////
+
 
 /**
  *  Start Mongo with Mongoose
